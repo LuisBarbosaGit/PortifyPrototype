da11a810203aa8c8f99fdcc70f9cbf0964c7c29a
LuisbarbosaGit <luisbarbosa.santos@outlook.com>
2025-05-15 15:01:32 -0300

    Pequenos ajustes

--- templates/template01/script.js
+++ templates/template01/script.js
@@ -1,5 +1,16 @@
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.getElementById('title').classList.add('show');
-    }, 300);
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title');
+
+const observer = new IntersectionObserver((items, observer) => {
+       items.forEach(item => {
+      if (item.isIntersecting) {
+        item.target.classList.add('show');
+        observer.unobserve(item.target); // para parar de observar após aparecer
+      }
     });
+  },  {
+    threshold: 0.2, // quando 20% do elemento estiver visível
+  });
+
+observer.observe(title)
+})
