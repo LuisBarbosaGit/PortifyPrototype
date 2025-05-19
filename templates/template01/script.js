88dcee5291286a0232ab67a31dacee254bb3c608
Luis Barbosa <111447471+LuisBarbosaGit@users.noreply.github.com>
2025-05-19 14:03:00 -0300

    Update script.js

--- templates/template01/script.js
+++ templates/template01/script.js
@@ -1,16 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const title = document.getElementById('title');
-
 const observer = new IntersectionObserver((items, observer) => {
-       items.forEach(item => {
-      if (item.isIntersecting) {
+  items.forEach((item, index) => {
+    if (item.isIntersecting) {
+      setTimeout(() => {
         item.target.classList.add('show');
         observer.unobserve(item.target); // para parar de observar após aparecer
-      }
-    });
-  },  {
-    threshold: 0.2, // quando 20% do elemento estiver visível
+      }, index * 1000);
+    }
   });
+},  {
+  threshold: 0.8, // quando 20% do elemento estiver visível
+});
 
-observer.observe(title)
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title');
+    observer.observe(title)
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const containt = document.querySelectorAll('#tec-cards');
+    setTimeout(() => {
+      showItem(containt)
+    }, 400);
+})
+
+//Função para enviar cada um dos cards com um intervalo de tempo entre si
+function showItem(container) {
+  let i = 1
+  container.forEach((item, index) =>{
+    observer.observe(item)
+    
+    })   
+  }
+
+function message() {
+  document.addEventListener('click', () =>{
+    window.location.href = "https://w.app/luisbarbosa"
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('scroll').addEventListener('click', () => {
+    const container = document.getElementById('tec-container');
+    if (container) {
+      container.scrollIntoView({behavior: "smooth"})
+    }
+  })
 })
